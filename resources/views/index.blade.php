a96d8221ceb7e90bcf5cdafb93cdea4056246f2e
Bob <bobcoderss@gmail.com>
2021-07-28 23:26:35 +0800

    Add 添加页面

---
+++ resources/views/index.blade.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html lang="">
+<head>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width,initial-scale=1">
+    <link rel="icon" href="/favicon.ico">
+    <title>Bob-Argon</title>
+    <link rel="stylesheet" href="https://unpkg.com/element-plus/lib/theme-chalk/index.css">
+    <link href="/css/app.e87024c8.css" rel="preload" as="style">
+    <link href="/css/chunk-vendors.4f06f28f.css" rel="preload" as="style">
+    <link href="/js/app.7acac5aa.js" rel="preload" as="script">
+    <link href="/js/chunk-vendors.2b6890ae.js" rel="preload" as="script">
+    <link href="/css/chunk-vendors.4f06f28f.css" rel="stylesheet">
+    <link href="/css/app.e87024c8.css" rel="stylesheet">
+</head>
+<body>
+<div id="app"></div>
+<script src="/js/chunk-vendors.2b6890ae.js"></script>
+<script src="/js/app.7acac5aa.js"></script>
+</body>
+</html>
